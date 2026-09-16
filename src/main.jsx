d925a89a6af5224ec4ede8220c9dd37ff9366075
hood07-ops <hood07@gmail.com>
2026-09-15 22:43:59 -0500

    feat(stability): protect app with global error recovery

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import ErrorBoundary from './ErrorBoundary.jsx'
 import PremiumMobileNav from './PremiumMobileNav.jsx'
 import { registerPowerFitPwa } from './pwa'
 
@@ -9,7 +10,9 @@ registerPowerFitPwa()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-    <PremiumMobileNav />
+    <ErrorBoundary>
+      <App />
+      <PremiumMobileNav />
+    </ErrorBoundary>
   </StrictMode>,
 )
