f61537ff2b8ea9f5c749f955d38762747bcc8b27
hood07-ops <hood07@gmail.com>
2026-09-15 22:30:39 -0500

    feat(ui): mount premium mobile navigation shell

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import PremiumMobileNav from './PremiumMobileNav.jsx'
 import { registerPowerFitPwa } from './pwa'
 
 registerPowerFitPwa()
@@ -9,5 +10,6 @@ registerPowerFitPwa()
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <PremiumMobileNav />
   </StrictMode>,
 )
